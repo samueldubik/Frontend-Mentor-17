faeb1cf3cf398e8a18aec98362660632d65f479e
Samuel Dubik <samuel.dubik@gmail.com>
2022-07-05 15:56:46 +0200

    final-fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <main class=" lg:w-[40vw] lg:mx-auto lg:my-[25vh] lg:h-[50vh] lg:flex ">
-    <img class=" lg:hidden " src="src/assets/image-product-mobile.jpg" alt="">
-    <img src="src/assets/image-product-desktop.jpg" class=" hidden lg:flex lg:w-1/2 lg:rounded-tl-xl lg:rounded-bl-xl lg:rounded-tr-none" alt="">
+    <img class=" lg:hidden " src="/src/assets/image-product-mobile.jpg" alt="">
+    <img src="/src/assets/image-product-desktop.jpg" class=" hidden lg:flex lg:w-1/2 lg:rounded-tl-xl lg:rounded-bl-xl lg:rounded-tr-none" alt="">
     <section class=" ">
       <h2 class=" ">PERFUME</h2>
       <h1 class=" ">Gabrielle Essence Eau De Parfum</h1>
@@ -14,7 +14,7 @@
       <h4 class=" ">$169.99</h4>
       </div>
       <div class=" flex justify-center items-center  bg-darkCyan mt-7 w-full h-14 rounded-lg text-white">
-        <img class=" h-1/3" src="product-preview-card-component-main/images/icon-cart.svg" alt="">
+        <img class=" h-1/3" src="/src/assets/icon-cart.svg" alt="">
         <h5 class=" ">Add to Cart</h5>
         </div>
     </section>
